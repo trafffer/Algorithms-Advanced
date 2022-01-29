3e8ee450f875c46b1c80b1922c7c77afbcdd877e
trafffer <sonya0@mail.bg>
2022-01-29 20:28:54 +0200

    This is the project for the course Algorithms Advanced.v2.

--- src/main/java/Dijkstra.java
+++ src/main/java/Dijkstra.java
@@ -1,10 +1,12 @@
+package main.java;
+
 import java.util.List;
 
 public class Dijkstra {
 
     public static List<Integer> dijkstraAlgorithm(int[][] graph, int sourceNode, int destinationNode) {
 
-        
+
 
 
 
